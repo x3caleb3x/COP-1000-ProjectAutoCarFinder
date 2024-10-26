bf0d939c81bcc6d35c10970c9700323552ca6a51
Caleb Dowdy <x3caleb3x@gmail.com>
2024-10-26 15:49:54 -0400

    main.py

--- main.py
+++ main.py
@@ -7,34 +7,6 @@ def vehicles():
   file = open("authorizedvehiclelist.txt", "r")
   response = file.read()
   print(response)
-#Function to Search for a Car
-def searchCar():
-  car = input("Please Enter the full Vehicle Name: ")
-  with open("authorizedvehiclelist.txt", 'r') as search:
-    filing = search.read()
-    if car in filing:
-      print(str(car)+ " is an authorized vehicle")
-    else:
-      print(str(car)+ " is not an authorized vehicle")
-#Function to Add Car
-def addCar():
-  newCar = input("Please Enter the full Vehicle name you would like to add: ")
-  with open("authorizedvehiclelist.txt", "a") as file:
-    file.write("\n")
-    file.write(newCar)
-    print("You have added " + str(newCar) + " as an authorized vehicle")
-#Function to Remove Car
-def removeCar():
-  deleteCar = input("Please Enter the full Vehicle name you would like to REMOVE: ")
-  assurance = input("Are you sure you want to remove " + str(deleteCar) + " from the Authorized Vehicle List? ")
-  if assurance == "yes":
-    with open("authorizedvehiclelist.txt", "r") as file:
-      lines = file.readlines()
-    with open("authorizedvehiclelist.txt", "w") as file:
-      for line in lines:
-        if line.strip("\n") != deleteCar:
-          file.write(line)
-    print("You have REMOVED " + str(deleteCar) + " as an authorized vehicle")
 
 #Menu
 while input != 5:
